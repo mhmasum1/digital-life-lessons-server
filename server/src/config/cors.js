557ef3dd09aeb4e8efc34290eb6f652b97947bbf
mhmasum1 <mhmasum231@gmail.com>
2026-03-11 18:00:32 +0600

    corsJs updated

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,25 +1,25 @@
+// const corsOptions = {
+//     origin: true,
+//     credentials: true,
+// };
+
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.SITE_DOMAIN,
+].filter(Boolean);
+
 const corsOptions = {
-    origin: true,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
 };
 
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   process.env.SITE_DOMAIN,
-// ].filter(Boolean);
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true);
-
-//     if (allowedOrigins.includes(origin)) {
-//       return callback(null, true);
-//     }
-
-//     return callback(new Error("Not allowed by CORS"));
-//   },
-//   credentials: true,
-// };
-
-// module.exports = { corsOptions };
+module.exports = { corsOptions };
